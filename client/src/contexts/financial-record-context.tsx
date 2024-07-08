f06ff33041a3b16fbc4a6e8c8e8e7bbfdb2030c7
AmanShakya0018 <amanshakya1808@gmail.com>
2024-07-08 18:03:54 +0530

    api

--- client/src/contexts/financial-record-context.tsx
+++ client/src/contexts/financial-record-context.tsx
@@ -32,7 +32,7 @@ export const FinancialRecordsProvider = ({
   const fetchRecords = async () => {
     if (!user) return;
     const response = await fetch(
-      `http://localhost:3001/financial-records/getAllByUserID/${user.id}`
+      `https://expense-tracker-server-five.vercel.app/financial-records/getAllByUserID/${user.id}`
     );
 
     if (response.ok) {
@@ -47,7 +47,7 @@ export const FinancialRecordsProvider = ({
   }, [user]);
 
   const addRecord = async (record: FinancialRecord) => {
-    const response = await fetch("http://localhost:3001/financial-records", {
+    const response = await fetch("https://expense-tracker-server-five.vercel.app/financial-records", {
       method: "POST",
       body: JSON.stringify(record),
       headers: {
@@ -65,7 +65,7 @@ export const FinancialRecordsProvider = ({
 
   const deleteRecord = async (id: string) => {
     const response = await fetch(
-      `http://localhost:3001/financial-records/${id}`,
+      `https://expense-tracker-server-five.vercel.app/financial-records/${id}`,
       {
         method: "DELETE",
       }
